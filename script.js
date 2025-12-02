a7d2cbb71d064039537dc8bffad464822e62f867
Kevin Rakholiya-Exhibyte <kevin.rakholiya@exhibytesolution.com>
2025-12-02 18:22:46 +0530

    complete pending task

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const request = indexedDB.open("todo_Database", 2)
 const STORE_NAME = "todo_store"
 let db;
+let undoStack = [];
+
 
 request.onupgradeneeded = (event) => {
     db = event.target.result
@@ -21,22 +23,28 @@ request.onerror = function (event) {
 
 
 function addTask(status) {
-    window.location.reload()
+
     const transaction = db.transaction(STORE_NAME, "readwrite");
     const store = transaction.objectStore(STORE_NAME);
     const taskInput = document.getElementById('taskInput').value;
-    console.log(taskInput);
     const taskObject = {
         task: taskInput,
         status: status
     };
 
-    console.log(taskObject);
-
     let addRequest = store.add(taskObject);
 
-    addRequest.onsuccess = function () {
+    addRequest.onsuccess = function (event) {
+        const newId = event.target.result
+
         console.log("Data added sucessfully");
+        undoStack.push({
+            type: "create",
+            id: newId
+        })
+        document.getElementById('taskInput').value = ""
+
+        LoadData();
 
     };
 
@@ -69,32 +77,51 @@ function renderBoard(tasks) {
         const taskDiv = document.createElement('div');
         taskDiv.classList.add('task-item');
         taskDiv.setAttribute('draggable', 'true');
+        taskDiv.id = `task-${task.id}`;
+
 
         taskDiv.innerHTML = `
             <span class="task-text">${task.task}</span>
-            <button class="update-task-btn" data-id="${task.id}" onclick="UpdateTask(${task.id})">Update</button>
+            <button class="update-task-btn" data-id="${task.id}" onclick="enableEdit(${task.id})")">Update</button>
             <button class="delete-task-btn" data-id="${task.id}" onclick="deleteTask(${task.id})">Delete</button>
-
         `;
         containers.appendChild(taskDiv);
+
+        taskDiv.addEventListener('dragstart', (e) => {
+            taskDiv.classList.add('is-dragging')
+            e.dataTransfer.setData("text/plain", taskDiv.id);
+
+        })
+
+        taskDiv.addEventListener('dragend', (e) => {
+            taskDiv.classList.remove('is-dragging')
+        })
     });
 }
+
 
 function deleteTask(id) {
     const transaction = db.transaction(STORE_NAME, "readwrite");
     const store = transaction.objectStore(STORE_NAME);
 
-    const request = store.delete(id)
-    console.log(request);
+    const DeletedData = store.get(id);
+    console.log(DeletedData);
 
+    DeletedData.onsuccess = () => {
+        const deletedItem = DeletedData.result
 
-    request.onsuccess = () => {
+        undoStack.push({
+            type: "delete",
+            data: deletedItem
+        })
+        store.delete(id)
         console.log("Data Deleted sucessfully");
+
         LoadData();
 
     }
 
-    request.onerror = (event) => {
+    DeletedData.onerror = (event) => {
         console.error('Error adding item:', event.target.error);
     };
 }
@@ -106,31 +133,79 @@ function UpdateTask(id, tasks) {
 
     getData.onsuccess = (event) => {
         const todoItem = event.target.result
+        console.log(todoItem);
 
         if (todoItem) {
-            console.log(todoItem);
             todoItem.task = tasks
-
-            const Update_Todo = store.put(todoItem)
-
-            Update_Todo.onsuccess = function () {
-                console.log("Todo item updated successfully:", todoItem);
-            };
-
-            Update_Todo.onerror = function () {
-                console.error("Error updating item:", putRequest.error);
-            }
+            store.put(todoItem)
         }
     }
 }
 
-function initializeDragAndDrop() {
-    const containers = document.querySelectorAll('.tasks-container');
-
-    containers.forEach(container => {
-        container.addEventListener('dragover', handleDragOver);
-        container.addEventListener('drop', handleDrop);
-        container.addEventListener('dragleave', handleDragLeave);
-    });
+function enableEdit(id) {
+    const taskItem = document.querySelector(`[data-id='${id}']`).parentNode
+    const taskText = taskItem.querySelector('.task-text')
+    taskText.contentEditable = true
+    const UpdateBtn = taskItem.querySelector(".update-task-btn")
+    UpdateBtn.textContent = 'save'
+    UpdateBtn.setAttribute('onclick', `saveTask (${id})`)
 }
 
+function saveTask(id) {
+    const taskItem = document.querySelector(`[data-id='${id}']`).parentNode
+    const taskText = taskItem.querySelector('.task-text').innerText
+    UpdateTask(id, taskText)
+    LoadData();
+}
+
+
+const dragColumns = document.querySelectorAll('.column')
+dragColumns.forEach((dragColumn) => {
+    dragColumn.addEventListener('dragover', (e) => {
+        e.preventDefault()
+    })
+
+    dragColumn.addEventListener('drop', (e) => {
+        e.preventDefault();
+        const taskId = e.dataTransfer.getData("text/plain");
+        const task = document.getElementById(taskId)
+        dragColumn.appendChild(task);
+    })
+})
+
+function undoAction() {
+    if (undoStack.length === 0) {
+        alert("Nothing to Undo")
+        console.log("nothing to undo");
+        return
+    }
+    const action = undoStack.pop()
+
+    switch (action.type) {
+        case "create":
+            undoCreate(action)
+            break;
+
+        case "delete":
+            undoDelete(action)
+            break;
+
+        default:
+            console.log("unknow Action", action);
+
+    }
+
+}
+
+function undoCreate(action) {
+    deleteTask(action.id)
+}
+
+function undoDelete(action) {
+    const transaction = db.transaction(STORE_NAME, "readwrite");
+    const store = transaction.objectStore(STORE_NAME);
+
+    store.put(action.data).onsuccess = () => {
+        LoadData()
+    }
+}
